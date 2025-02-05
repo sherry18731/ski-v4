d909e4374736cafeb90d78e2c7a7fa854da2372b
Chewsday <z647623@gmail.com>
2025-02-05 14:33:24 +0800

    公版V4-4

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.scss';
 
 
 
-import Home from './pages/home/homeComponents/Home';       
+import Home from './pages/home/Home';       
 import Header from './layout/Header';
 import Footer from './layout/Footer';
 
